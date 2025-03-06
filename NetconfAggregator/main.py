610666ccb73052f5f9c9b4418884f7357849dc0e
Joar Heimonen <contact@joar.me>
2025-03-06 18:31:53 +0100

    feat: initial commit

---
+++ NetconfAggregator/main.py
@@ -0,0 +1,29 @@
+import argparse
+from NetconfAggregator.settings import settings_exist, create_settings, edit_settings
+
+import tntapi
+from yangcli import yangcli
+from lxml import etree
+
+def main():
+    if not settings_exist():
+        create_settings()
+
+    parser = argparse.ArgumentParser(description="NetconfAggregator")
+
+    parser.add_argument(
+        "--settings",
+        "-s",
+        action="store_true",
+        help="Edit the settings",
+    )
+
+    args = parser.parse_args()
+
+    if args.settings:
+        edit_settings()
+        return
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+requires litenc: ```pip install git+https://github.com/Slenderman00/litenc.git#egg=litenc```
+requires tntapi: ```pip install git+https://github.com/Slenderman00/litenc.git@tntapi#egg=tntapi```
+requires yangcli: ```pip install git+https://github.com/Slenderman00/yuma123.git@python-yangcli#egg=yangcli```
